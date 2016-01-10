cdf3967812793d367baf3ea8d558c9651017e6f6
Nathan Thomas <thomasnathaniell@gmail.com>
2016-01-10 18:14:04 -0500

    Reflect on the horror we created

--- java-horror-story/src/Main.java
+++ java-horror-story/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Random;
 
-public class Main
-{
-
-	public static void main(String[] args)
-	{
-		System.out.println("AAAAAAAAAGH THE HORROR!");
+public class Main {
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		boolean prob = random.nextBoolean();
+		if (prob) {
+			System.out.println("AAAAAAAAAGH THE HORROR!");
+		} else {
+			System.out.println("WHAT HAVE WE DONE!");
+		}
 	}
 
 }
